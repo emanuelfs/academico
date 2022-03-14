9cce21006d48035bda9e12c03575ff82febcc077
Emanuel Ferreira da Silva <emanuel.silva@consistem.com.br>
2022-03-14 20:25:33 -0300

    Alteração de teste

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+// builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
 builder.Services.AddDbContext<academicoContext>(opt => opt.UseSqlServer("Server=localhost;Database=academico;Trusted_Connection=True;TrustServerCertificate=True;User Id=sa;Password=P@ssw0rd;"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
